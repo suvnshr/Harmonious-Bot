6b3f6d5f7ca0b77b2ad7e426068c167502bb97c5
= <36293610+suvansh-rana@users.noreply.github.com>
2020-02-07 21:21:24 +0530

    Setting the upper of limit of message delettion as 100

--- commands/clear_channel.js
+++ commands/clear_channel.js
@@ -3,11 +3,11 @@ module.exports = {
     description: 'Deletes the specified number of messages of this channel, defaults to 100',
     execute(message, args) {
 
-        const messagesToDel = Number(args[0]) || 100;
+        const messagesToDel = Number(args[0]) || 99;
 
         async function clear() {
             message.delete();
-            const fetched = await message.channel.fetchMessages({ limit: messagesToDel + 1 });
+            const fetched = await message.channel.fetchMessages({ limit: messagesToDel });
             message.channel.bulkDelete(fetched);
         }
 
